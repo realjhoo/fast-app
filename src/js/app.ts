a57afa002d84c34224e755146878cca5e7987d7d
Jerry L Hoover <jerry.hoover@ymail.com>
2021-07-23 18:38:57 -0500

    Initial commit

---
+++ src/js/app.ts
@@ -0,0 +1,20 @@
+/*
+Generate and inject markup
+
+Determine which watch
+  1st = 8pm-12am  (8pm -> red, 12am -> grey)
+  2nd = 12am - 4am (8pm -> red, 4am -> grey)
+  3rd = 4am-8am (8pm -> red, 8am -> grey)
+  4th = 8am-12pm (noon) (8pm -> red, 12pm -> grey)
+  5th = 12pm - 4pm (12pm -> green, 4pm -> grey)
+  6th = 4pm - 8pm (12pm -> red, 8pm -> grey)
+
+Determine time til fasting state changes
+  Place countdown in center display
+  Center display (red 8-12, green 12-8)
+
+Determine Day for Sweets
+  Decide which days sweets are allowed
+Determine Day for Walks
+  Decide which days I should walk
+*/
